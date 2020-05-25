f070f20a31fde3d83719199bdb795a582752b811
Christophe Borios <63300503+ChristopheBorios@users.noreply.github.com>
2020-05-25 10:10:23 +0200

    Update connect.php

--- inc/connect.php
+++ inc/connect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $db = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+    $db = new PDO('mysql:host=localhost;dbname=', 'root', '');
     $db->exec('SET NAMES "UTF8"');
 }catch(PDOException $e){
     echo "Erreur : " . $e->getMessage();
